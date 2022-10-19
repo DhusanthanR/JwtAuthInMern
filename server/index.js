5a7cc5d0c0e285a2f4e79394db46cf4144222a11
Dhusanthan R <52322074+DhusanthanR@users.noreply.github.com>
2022-10-19 08:29:23 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connection = require('./db');
+const userRoute = require('./routers/users');
+const authRoute = require('./routers/auth');
+
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users',userRoute);
+app.use('/api/auth',authRoute);
+
+const port = process.env.PORT || 8080;
+app.listen( port, () => console.log(`Listening on port ${port}...!`) )
+
